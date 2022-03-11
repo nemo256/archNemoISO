3c3295718759de5c7f0331e1cf73fda9bf51b6d4
Amine Neggazi <neggazimedlamine@gmail.com>
2022-03-11 13:15:38 +0100

    Updated iso version!

--- profiledef.sh
+++ profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="archNemo"
-iso_label="archNemo_$(date +%Y%m)"
+iso_label="archNemo_0.0.1"
 iso_publisher="Amine Neggazi <https://amineneggazi.vercel.app>"
 iso_application="Arch Nemo"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="0.0.1"
 install_dir="archNemo"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
