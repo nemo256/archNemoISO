0244fe1d67cd42a0972687f014362c61a50de631
Amine Neggazi <neggazimedlamine@gmail.com>
2022-03-11 09:39:22 +0100

    Updated custom iso informations!

--- profiledef.sh
+++ profiledef.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-baseline"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux baseline"
+iso_name="archNemo"
+iso_label="archNemo_$(date +%Y%m)"
+iso_publisher="Amine Neggazi <https://amineneggazi.vercel.app>"
+iso_application="Arch Nemo"
 iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+install_dir="archNemo"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-airootfs_image_tool_options=('-zlz4hc,12')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
 )
