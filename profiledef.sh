34e329440424e1c18edf3461f61af65779485c75
Amine Neggazi <neggazimedlamine@gmail.com>
2022-03-11 19:29:26 +0100

    Added install script (executes on boot)!

--- profiledef.sh
+++ profiledef.sh
@@ -16,6 +16,7 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/install.sh"]="0:0:755"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
